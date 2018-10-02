71a18b4b255f26115ca5ebe35c79c5d7e9721bb1
pokyah <thomas.goossens@protonmail.com>
2018-10-02 14:23:28 +0000

    updating agrometeor template

--- R/epurate.R
+++ R/epurate.R
@@ -1,5 +1,27 @@
 library(rmarkdown)
 
+agrometeor <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "agrometeor" ,"resources", "style.css", package = "epuRate")
+  footer <- system.file("rmarkdown", "templates", "agrometeor" ,"resources", "footer.html", package = "epuRate")
+  header <- system.file("rmarkdown", "templates", "agrometeor" ,"resources", "header.html", package = "epuRate")
+  template <- system.file("rmarkdown", "templates", "agrometeor" ,"resources", "template_agrometeor.html", package = "epuRate")
+
+  # call the base html_document function
+  rmarkdown::html_document( theme= "lumen",
+                               template= template,
+                               css= css,
+                               toc= toc,
+                               toc_float= TRUE,
+                               toc_depth= 2,
+                               number_sections= number_sections,
+                               df_print = "paged",
+                               code_folding = code_folding,
+                               includes = includes(before_body = header)
+                            )
+}
+
 epurate <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
 
   # get the locations of resource files located within the package
